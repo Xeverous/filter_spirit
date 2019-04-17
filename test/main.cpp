8ed522df2abed6d5503955bb8e7cbc2c528f3fea
Xeverous <20820409+Xeverous@users.noreply.github.com>
2019-04-17 15:32:19 +0200

    CMake and tests refactor
    - added build to .gitignore
    - Filter Spirit now builds as shared library that is reused by main and test executable
    - rewritten existing tests from Google Test to Boost Test
    - fixed various compilation issues
    - improvements to CMakeLists.txt

--- test/main.cpp
+++ test/main.cpp
@@ -2,6 +2,10 @@
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 
+/*
+ * We do not write main function - it's already provided by
+ * the Boost Test library. Add only a sanity check that everything works.
+ */
 BOOST_AUTO_TEST_CASE(sanity_check)
 {
 	BOOST_TEST(true);
