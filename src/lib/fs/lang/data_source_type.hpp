c962f59fbb28131e5bc07a5067f60c1ca883ec57
Xeverous <20820409+Xeverous@users.noreply.github.com>
2020-09-17 07:18:37 +0200

    minor API adjustments

--- src/lib/fs/lang/data_source_type.hpp
+++ src/lib/fs/lang/data_source_type.hpp
@@ -6,9 +6,6 @@
 namespace fs::lang
 {
 
-// TODO Should these really be a part of lang namespace?
-// They are not a part of FS language.
-// Item price APIs are more close to the network.
 enum class data_source_type { none, poe_watch, poe_ninja };
 
 std::string_view to_string(data_source_type data_source) noexcept;
