4cb160e4219a8da7377829983a128264576990e6
Xeverous <20820409+Xeverous@users.noreply.github.com>
2020-10-18 15:13:00 +0200

    initial GUI sample based on Magnum bootstrap project

---
+++ src/gui/main.cpp
@@ -0,0 +1,28 @@
+#include <Magnum/GL/DefaultFramebuffer.h>
+#include <Magnum/Platform/Sdl2Application.h>
+
+using namespace Magnum;
+
+class MyApplication: public Platform::Application {
+	public:
+		explicit MyApplication(const Arguments& arguments);
+
+	private:
+		void drawEvent() override;
+};
+
+MyApplication::MyApplication(const Arguments& arguments):
+	Platform::Application{arguments}
+{
+	// TODO: Add your initialization code here
+}
+
+void MyApplication::drawEvent() {
+	GL::defaultFramebuffer.clear(GL::FramebufferClear::Color);
+
+	// TODO: Add your drawing code here
+
+	swapBuffers();
+}
+
+MAGNUM_APPLICATION_MAIN(MyApplication)
