33baa03e5e17a4dd2b792ef89f3acbf8e2b2b342
Xeverous <20820409+Xeverous@users.noreply.github.com>
2020-10-31 15:05:37 +0100

    GUI: version information improvements

--- src/gui/fs/gui/windows/version_info_window.cpp
+++ src/gui/fs/gui/windows/version_info_window.cpp
@@ -7,20 +7,51 @@
 
 #include <imgui.h>
 
+#include <SDL_version.h>
+
 #include <boost/version.hpp>
 #include <boost/config.hpp>
 
 #include <initializer_list>
+#include <string>
+
+namespace {
+
+std::string sdl_version_string()
+{
+	/*
+	 * Note: SDL has 2 ways of obtaining version information:
+	 * - SDL_GetVersion - library call
+	 * - SDL_VERSION - macro
+	 *
+	 * The first one returns version which the program is linked against,
+	 * the second one returns version which the program was compiled with.
+	 *
+	 * FS does not come with SDL bundled with it (except on Windows for convenience and ABI) so it is
+	 * better to query version information at runtime, because the library can be upgraded anytime.
+	 */
+	SDL_version ver;
+	SDL_GetVersion(&ver);
+	return std::to_string(ver.major) + "." + std::to_string(ver.minor) + "." + std::to_string(ver.patch);
+}
+
+}
 
 namespace fs::gui {
+
+version_info_window::version_info_window()
+: imgui_window("Version information")
+, _fs_version(to_string(fs::version::current()))
+, _boost_version(std::to_string(BOOST_VERSION / 100000) + "." + std::to_string(BOOST_VERSION / 100 % 1000))
+, _opengl_version(Magnum::GL::Context::current().versionString())
+, _sdl_version(sdl_version_string())
+, _cpp_version(std::to_string(__cplusplus / 100))
+{
+}
 
 void version_info_window::draw_contents()
 {
 	constexpr auto line_width = 300;
-	static const std::string fs_version = to_string(fs::version::current());
-	static const std::string opengl_version = Magnum::GL::Context::current().versionString();
-	static const std::string boost_version = std::to_string(BOOST_VERSION / 100000) + "." + std::to_string(BOOST_VERSION / 100 % 1000);
-	static const std::string cpp_version = std::to_string(__cplusplus / 100);
 	// __DATE__ has ugly "Mmm dd yyyy" format for legacy reasons, reorder it to ISO 8601
 	const auto build_date = {
 		__DATE__[7], __DATE__[8], __DATE__[9], __DATE__[10], ' ',
@@ -32,11 +63,11 @@ void version_info_window::draw_contents()
 
 	ImGui::Text("Filter Spirit version:");
 	ImGui::SameLine(line_width);
-	ImGui::Text(fs_version.c_str());
+	ImGui::Text(_fs_version.c_str());
 
 	ImGui::Text("Boost version:");
 	ImGui::SameLine(line_width);
-	ImGui::Text(boost_version.c_str());
+	ImGui::Text(_boost_version.c_str());
 
 	ImGui::Text("Magnum version:");
 	ImGui::SameLine(line_width);
@@ -52,11 +83,15 @@ void version_info_window::draw_contents()
 
 	ImGui::Text("OpenGL version:");
 	ImGui::SameLine(line_width);
-	ImGui::Text(opengl_version.c_str());
+	ImGui::Text(_opengl_version.c_str());
 
-	ImGui::Text("\n\n\n"); // TODO: find a better way for vertical space
+	ImGui::Text("SDL version:");
+	ImGui::SameLine(line_width);
+	ImGui::Text(_sdl_version.c_str());
 
-	ImGui::Text("build information:");
+	ImGui::Dummy({0.0f, ImGui::GetFontSize() * 2.0f});
+
+	ImGui::Text("Build information:");
 
 	ImGui::Text("build date:");
 	ImGui::SameLine(line_width);
@@ -64,7 +99,7 @@ void version_info_window::draw_contents()
 
 	ImGui::Text("C++ standard:");
 	ImGui::SameLine(line_width);
-	ImGui::Text(cpp_version.c_str());
+	ImGui::Text(_cpp_version.c_str());
 
 	ImGui::Text("C++ compiler:");
 	ImGui::SameLine(line_width);
