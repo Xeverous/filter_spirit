622d8d5cc5aaa0348a6b2d2bf354133a0518cb4f
Xeverous <20820409+Xeverous@users.noreply.github.com>
2020-10-31 15:05:37 +0100

    GUI: version information improvements

--- src/gui/fs/gui/windows/version_info_window.hpp
+++ src/gui/fs/gui/windows/version_info_window.hpp
@@ -2,18 +2,24 @@
 
 #include <fs/gui/imgui_window.hpp>
 
+#include <string>
+
 namespace fs::gui {
 
 class version_info_window : public imgui_window
 {
 public:
-	version_info_window()
-	: imgui_window("Version information")
-	{
-	}
+	version_info_window();
 
 protected:
 	void draw_contents() override;
+
+private:
+	const std::string _fs_version;
+	const std::string _boost_version;
+	const std::string _opengl_version;
+	const std::string _sdl_version;
+	const std::string _cpp_version;
 };
 
 }
