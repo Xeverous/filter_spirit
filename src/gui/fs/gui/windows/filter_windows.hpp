5471e2faf470a5072d269c94a9d7e5ed3a4217f5
Xeverous <20820409+Xeverous@users.noreply.github.com>
2020-11-10 16:19:43 +0100

    GUI: first common filter windows functionality

--- src/gui/fs/gui/windows/filter_windows.hpp
+++ src/gui/fs/gui/windows/filter_windows.hpp
@@ -16,17 +16,24 @@ class application;
 class real_filter_window : public imgui_window
 {
 public:
-	real_filter_window(std::string path)
-	: imgui_window(std::move(path))
+	real_filter_window(application& app, std::string path);
+
+	const std::string& real_filter_path() const
 	{
-		open();
+		return name();
 	}
 
 protected:
 	void draw_contents() override;
 
 private:
+	// use string instead of string_view because filesystem wants C-strings
+	void reload_real_filter_file(const std::string& path);
 
+	std::reference_wrapper<application> _application; // plain reference type would make this class non-moveable
+	gui_logger _logger;
+	std::string _status;
+	std::optional<real_filter_state> _state; // empty if loading file failed
 };
 
 class spirit_filter_window : public imgui_window
