f9c941867eacf29ce455361121826edbd45e3079
Xeverous <20820409+Xeverous@users.noreply.github.com>
2020-11-14 17:23:12 +0100

    move logger from filter window to filter state

--- src/gui/fs/gui/windows/filter_windows.cpp
+++ src/gui/fs/gui/windows/filter_windows.cpp
@@ -1,91 +1,37 @@
 #include <fs/gui/windows/filter_windows.hpp>
 #include <fs/gui/windows/filter/filter_state.hpp>
 #include <fs/gui/application.hpp>
-#include <fs/utility/file.hpp>
 
 #include <imgui.h>
-
-namespace {
-
-using namespace fs;
-using namespace fs::gui;
-
-void reload_filter_helper(const std::string& path, gui_logger& logger, std::string& status, detail::filter_state_base& state)
-{
-	std::optional<std::string> file_content = utility::load_file(path, logger.logger());
-	if (!file_content) {
-		status = "loading failed";
-		state.new_source(std::nullopt, logger.logger());
-		return;
-	}
-
-	status = "filter loaded, ";
-
-	state.new_source(std::move(file_content), logger.logger());
-
-	if (const auto& rep = state.filter_representation(); rep.has_value()) {
-		status += std::to_string((*rep).blocks.size());
-		status += " blocks";
-	}
-	else {
-		status += "parsing/compiling failed";
-	}
-}
-
-} // namespace
 
 namespace fs::gui {
 
 real_filter_window::real_filter_window(application& app, std::string path)
 : imgui_window(std::move(path))
 , _application(std::ref(app))
-, _logger(app.font_settings())
+, _state(app)
 {
 	open();
-	reload_real_filter_file(real_filter_path());
+	_state.reload_source_file(real_filter_path());
 }
 
 void real_filter_window::draw_contents()
 {
-	if (ImGui::Button("reload real filter file"))
-		reload_real_filter_file(real_filter_path());
-
-	ImGui::SameLine();
-	ImGui::TextUnformatted(_status.c_str());
-
-	_logger.draw();
-	_state.draw_interface(_application.get());
-}
-
-void real_filter_window::reload_real_filter_file(const std::string& path)
-{
-	reload_filter_helper(path, _logger, _status, _state);
+	_state.draw_interface(real_filter_path(), _application.get());
 }
 
 spirit_filter_window::spirit_filter_window(application& app, std::string path)
 : imgui_window(std::move(path))
 , _application(std::ref(app))
-, _logger(app.font_settings())
+, _state(app)
 {
 	open();
-	reload_spirit_filter_file(spirit_filter_path());
+	_state.reload_source_file(spirit_filter_path());
 }
 
 void spirit_filter_window::draw_contents()
 {
-	if (ImGui::Button("reload spirit filter template file"))
-		reload_spirit_filter_file(spirit_filter_path());
-
-	ImGui::SameLine();
-	ImGui::TextUnformatted(_status.c_str());
-
-	_logger.draw();
-	_state.draw_interface(_application.get());
-}
-
-void spirit_filter_window::reload_spirit_filter_file(const std::string& path)
-{
-	reload_filter_helper(path, _logger, _status, _state);
+	_state.draw_interface(spirit_filter_path(), _application.get());
 }
 
 }
