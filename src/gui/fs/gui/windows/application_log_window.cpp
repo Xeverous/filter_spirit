6685643d9687c50a133a49397087cfb96e755a1f
Xeverous <20820409+Xeverous@users.noreply.github.com>
2020-11-14 17:35:30 +0100

    gui_logger: occupy whole space, buttons moved up

--- src/gui/fs/gui/windows/application_log_window.cpp
+++ src/gui/fs/gui/windows/application_log_window.cpp
@@ -9,8 +9,7 @@ void application_log_window::draw_contents()
 	ImGui::TextUnformatted(
 		"Here you can find logs for the entire application.\n"
 		"If some errors appear here, something must went horribly wrong.");
-
-	_logger.draw(-1.5f); // take as much space as possible but leave 1.5 lines of space for buttons
+	_logger.draw();
 }
 
 }
