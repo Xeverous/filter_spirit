7eb3f3915cfd54fa25418ec22a3792b93e3d9568
Xeverous <20820409+Xeverous@users.noreply.github.com>
2020-11-23 15:13:31 +0100

    gui::aux - added multiline_text_input and colored buttons

--- src/gui/fs/gui/auxiliary/widgets.hpp
+++ src/gui/fs/gui/auxiliary/widgets.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string_view>
+#include <vector>
 
 #include <imgui.h>
 
@@ -12,5 +13,31 @@ inline ImVec2 measure_text_line(std::string_view line, int font_size, const ImFo
 }
 
 void on_hover_text_tooltip(std::string_view text);
+
+bool button_positive(const char* str);
+bool button_negative(const char* str);
+
+class multiline_text_input
+{
+public:
+	multiline_text_input()
+	{
+		_buf.resize(1024);
+	}
+
+	void set_text(std::string_view text);
+
+	bool draw(ImVec2 size = {});
+
+	const char* c_str() const
+	{
+		return _buf.data();
+	}
+
+private:
+	// not std::string because for multi-line text string's small buffer
+	// optimization is more code complexity than benefit
+	std::vector<char> _buf;
+};
 
 }
