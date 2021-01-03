7908d300b79ea291c1dda98fff49c7d03f8ca271
Xeverous <20820409+Xeverous@users.noreply.github.com>
2021-01-03 17:18:43 +0100

    better main function for emscripten builds

--- src/gui/fs/main.cpp
+++ src/gui/fs/main.cpp
@@ -1,7 +1,11 @@
 #include <fs/gui/application.hpp>
 
-int main(int argc, char** argv)
-{
-	fs::gui::application app({argc, argv});
-	return app.exec();
-}
+/*
+ * This macro simply creates an object of specified type (giving {argc, argv} to ctor)
+ * and calls .exec() on it.
+ *
+ * For Emscripten build, the macro instantiates the application instance as a global variable
+ * instead of a local variable inside main(). This is in order to support the idle-efficient main
+ * loop, as otherwise the local scope would end before any event callback has a chance to happen
+ */
+MAGNUM_APPLICATION_MAIN(fs::gui::application)
